dc188c2822688ae67712df000b439ac90ccdadb6
Dhameliya Yash <140818500+yashdhameliya28@users.noreply.github.com>
2024-09-17 18:23:05 +0530

    Add files via upload

---
+++ DBMS/LAB 9/SUB QUERY.sql
@@ -0,0 +1,132 @@
+USE DARSHAN
+
+CREATE TABLE STUDENT_DATA
+(	
+	SNO INT,
+	SNAME VARCHAR(10),
+	CITY VARCHAR(15),
+	DID INT
+)
+
+INSERT INTO STUDENT_DATA(SNO,SNAME, CITY, DID)
+VALUES	(101,'RAJU','RAJKOT',10),
+		(102,'Amit', 'Ahmedabad', 20),
+		(103 ,'Sanjay' ,'Baroda' ,40),
+		(104, 'Neha',' Rajkot', 20),
+		(105, 'Meera', 'Ahmedabad', 30),
+		(106, 'Mahesh', 'Baroda', 10)
+
+SELECT * FROM STUDENT_DATA
+
+CREATE TABLE ACADEMIC
+(
+	RNO INT,
+	SPI DECIMAL(2,1),
+	BKLOG INT
+)
+
+INSERT INTO ACADEMIC(RNO,SPI,BKLOG)
+VALUES	(101,8.8 ,0),
+		(102 ,9.2 ,2),
+		(103 ,7.6, 1),
+		(104 ,8.2 ,4),
+		(105 ,7.0, 2),
+		(106 ,8.9 ,3)
+
+SELECT * FROM ACADEMIC
+
+CREATE TABLE DEPARTMENT
+(
+	DID INT,
+	DNAME VARCHAR(15)
+)
+
+INSERT INTO DEPARTMENT(DID, DNAME)
+VALUES	(10, 'Computer'),
+		(20, 'Electrical'),
+		(30, 'Mechanical'),
+		(40, 'Civil')
+
+SELECT * FROM DEPARTMENT
+
+--Part – A:
+
+--1. Display details of students who are from computer department.
+SELECT SNAME
+FROM STUDENT_DATA
+WHERE DID = (SELECT DID FROM DEPARTMENT WHERE DNAME = 'COMPUTER')
+
+--2. Displays name of students whose SPI is more than 8.
+SELECT SNAME
+FROM STUDENT_DATA
+WHERE SNO = (SELECT RNO FROM ACADEMIC WHERE SPI > 8)
+
+--3. Display details of students of computer department who belongs to Rajkot city.
+SELECT *
+FROM STUDENT_DATA
+WHERE CITY = 'RAJKOT'
+AND DID = (SELECT DID
+			FROM DEPARTMENT
+			WHERE DNAME = 'COMPUTER')
+
+--4. Find total number of students of electrical department.
+SELECT COUNT(SNO) 
+FROM STUDENT_DATA 
+WHERE DID = (SELECT DID 
+			FROM DEPARTMENT 
+			WHERE DNAME = 'ELECTRICAL')
+
+--5. Display name of student who is having maximum SPI.
+SELECT *
+FROM STUDENT_DATA
+WHERE SNO = (SELECT RNO
+			FROM ACADEMIC
+			WHERE SPI = (SELECT MAX(SPI)
+			FROM ACADEMIC))
+
+--6. Display details of students having more than 1 backlog.
+SELECT * 
+FROM STUDENT_DATA 
+WHERE SNO IN (SELECT RNO FROM ACADEMIC WHERE BKLOG > 1 )
+
+--Part – B:
+--1. Display name of students who are either from computer department or from mechanical department.
+SELECT * 
+FROM STUDENT_DATA
+WHERE DID IN 
+(SELECT DID 
+FROM DEPARTMENT 
+WHERE DNAME = 'COMPUTER' OR DNAME = 'MECHANICAL')
+
+--2. Display name of students who are in same department as 102 studying in.
+
+--Part – C:
+--1. Display name of students whose SPI is more than 9 and who is from electrical department.
+SELECT SNAME 
+FROM STUDENT_DATA 
+WHERE SNO = (SELECT RNO 
+			FROM ACADEMIC 
+			WHERE SPI = (SELECT SPI 
+						FROM ACADEMIC 
+						WHERE SPI > 9)) AND DID = (SELECT DID 
+													FROM DEPARTMENT 
+													WHERE DNAME = 'ELECTRICAL')
+
+--2. Display name of student who is having second highest SPI.
+SELECT SNAME 
+FROM STUDENT_DATA 
+WHERE SNO = (SELECT RNO 
+			FROM ACADEMIC 
+			WHERE SPI = (SELECT MAX(SPI) 
+						FROM ACADEMIC 
+						WHERE SPI < (SELECT MAX(SPI) 
+										FROM ACADEMIC )))
+
+--3. Display city names whose students branch wise SPI is 9.2
+SELECT CITY 
+FROM STUDENT_DATA 
+WHERE SNO = (SELECT RNO 
+			FROM ACADEMIC 
+			WHERE SPI = 9.2 ) AND DID IN (SELECT DID FROM 
+											DEPARTMENT 
+											GROUP BY DID )
